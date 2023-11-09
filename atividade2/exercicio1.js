ee521172003c28cdb1c4a5c9ba268474be2bcfde
Beatriz Rodrigues <79415707+Dorakio@users.noreply.github.com>
2023-11-08 21:42:52 -0300

    Create exercicio1.js

---
+++ atividade2/exercicio1.js
@@ -0,0 +1,18 @@
+var i = 3;
+var j = 6;
+
+//adição
+var resultado = i + j;
+console.log("Resultado da adição entre " + i + " e " + j + " = " + resultado);
+
+//subtração
+var resultado = i - j;
+console.log("Resultado da subtração entre " + i + " e " + j + " = " + resultado);
+
+//multiplicação
+var resultado = i * j;
+console.log("Resultado da multiplicação entre " + i + " e " + j + " = " + resultado);
+
+//divisão
+var resultado = i + j;
+console.log("Resultado da divisão entre " + i + " e " + j + " = " + resultado);
