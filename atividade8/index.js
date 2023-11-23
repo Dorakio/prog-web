ac71a73eefb7fe1616944d200cc25777248a24b8
Beatriz Rodrigues <79415707+Dorakio@users.noreply.github.com>
2023-11-23 14:26:03 -0300

    Create index.js

---
+++ atividade8/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + './source/views');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use("/", require('./source/routes/calcuRoutes'));
+
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log("App rodando na porta " + PORT);
+})
